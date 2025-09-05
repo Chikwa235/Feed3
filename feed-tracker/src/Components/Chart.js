967f8afb9573370031b9650a24edda3618b8fbc9
Chikwa235 <chisapachikwa25@gmail.com>
2025-09-05 17:29:32 +0200

    feat: compute food type counts in FoodTypeChart useEffect

--- feed-tracker/src/Components/Chart.js
+++ feed-tracker/src/Components/Chart.js
@@ -4,3 +4,11 @@ import Chart from 'chart.js/auto';
 const FoodTypeChart = ({ foodData }) => {
   const chartRef = useRef(null);
   const chartInstanceRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = chartRef.current.getContext('2d');
+    const typeCounts = {};
+
+    foodData.forEach(item => {
+      typeCounts[item.type] = (typeCounts[item.type] || 0) + 1;
+    });
