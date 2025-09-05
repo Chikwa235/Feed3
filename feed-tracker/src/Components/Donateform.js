2710568890bb0a2069bac42355109b10190fa6e9
Chikwa235 <chisapachikwa25@gmail.com>
2025-09-05 17:39:25 +0200

    feat: add state management to DonateForm component

--- feed-tracker/src/Components/Donateform.js
+++ feed-tracker/src/Components/Donateform.js
@@ -1,1 +1,7 @@
 import React, { useState } from "react";
+
+const DonateForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [description, setDescription] = useState("");
+  const [error, setError] = useState("");
