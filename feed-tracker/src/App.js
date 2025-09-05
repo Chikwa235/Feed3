56e34c1b4a9656b05af34b16c304fbe9b5923208
Chikwa235 <chisapachikwa25@gmail.com>
2025-09-05 18:40:28 +0200

    feat: add custom marker for user location with popup on map

--- feed-tracker/src/App.js
+++ feed-tracker/src/App.js
@@ -99,3 +99,19 @@ useEffect(() => {
 const setUserLocation = (lat, lng) => {
   if (!isMounted) return;
   map.setView([lat, lng], 15);
+
+    // Correct marker with proper icon
+  L.marker([lat, lng], {
+    icon: L.icon({
+      iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+      shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    })
+  })
+  .addTo(map)
+  .bindPopup("You are here!")
+  .openPopup();
+};
