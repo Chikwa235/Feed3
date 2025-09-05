fd3fc685d7fa8b055a444f56b20b34e7ecc431bb
Chikwa235 <chisapachikwa25@gmail.com>
2025-09-05 17:33:46 +0200

    feat: add cleanup and export FoodTypeChart component

--- feed-tracker/src/Components/Chart.js
+++ feed-tracker/src/Components/Chart.js
@@ -46,3 +46,17 @@ const FoodTypeChart = ({ foodData }) => {
         },
       },
     });
+
+     // Cleanup on component unmount
+    return () => {
+      if (chartInstanceRef.current) {
+        chartInstanceRef.current.destroy();
+      }
+    };
+  }, [foodData]);
+
+  return <canvas ref={chartRef} id="foodTypeChart" width="400" height="200"></canvas>;
+};
+
+export default FoodTypeChart;
+
