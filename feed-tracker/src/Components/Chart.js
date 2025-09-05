7a866b64d99951d109be86f5956fbc0a8f3ea0c4
Chikwa235 <chisapachikwa25@gmail.com>
2025-09-05 17:28:39 +0200

    feat: initialize FoodTypeChart component with refs

--- feed-tracker/src/Components/Chart.js
+++ feed-tracker/src/Components/Chart.js
@@ -1,2 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
+
+const FoodTypeChart = ({ foodData }) => {
+  const chartRef = useRef(null);
+  const chartInstanceRef = useRef(null);
