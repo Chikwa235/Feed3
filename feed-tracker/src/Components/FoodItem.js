0a87f8f6b8765c1fce003ab5cbc3492a4d6ae70d
Chikwa235 <chisapachikwa25@gmail.com>
2025-09-05 17:51:43 +0200

    feat: add FoodItem component with calculateDaysLeft utility

--- feed-tracker/src/Components/FoodItem.js
+++ feed-tracker/src/Components/FoodItem.js
@@ -1,2 +1,8 @@
 import React from 'react';
 
+const FoodItem = ({ item, index, removeFoodItem }) => {
+  const calculateDaysLeft = (date) => {
+    const now = new Date();
+    const exp = new Date(date);
+    return Math.ceil((exp - now) / (1000 * 60 * 60 * 24));
+  };
