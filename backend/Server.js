5a7589ebe2cafbb4d0302ce3b5cd13e871b8ca05
Chikwa235 <chisapachikwa25@gmail.com>
2025-09-06 15:54:27 +0200

    feat: connect backend server to MongoDB using Mongoose

--- backend/Server.js
+++ backend/Server.js
@@ -10,3 +10,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
