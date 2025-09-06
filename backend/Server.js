8140e4fdb21db5914c6b63f72896a0718c211eea
Chikwa235 <chisapachikwa25@gmail.com>
2025-09-06 15:49:06 +0200

    feat: setup backend folder with server.js and install dependencies

---
+++ backend/Server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
